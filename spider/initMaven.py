e3bffaa74d8ea1ba6b6293c19eda4288f21ff3ed
wfqtyhj@gmail.com <wfqtyhj@gmail.com>
2019-11-27 18:21:54 +0800

    demo

---
+++ spider/initMaven.py
@@ -0,0 +1,33 @@
+import subprocess
+import sys
+
+
+def initMaven(mvn, url):
+    child = subprocess.Popen(mvn, shell=True, cwd=url, stdout=subprocess.PIPE)
+    return child.stdout.read()
+
+
+def getResult(mvn, url):
+    if mvn is None or mvn.strip() == "":
+        mvn = "mvn archetype:generate -DgroupId=com.spdb -DartifactId=test -DpackageName=com.spdb " \
+              "-Dversion=0.0.1-SNAPSHOT -DapplicationName=component -DarchetypeArtifactId=maven-archetype-webapp " \
+              "-DinteractiveMode=false "
+    return initMaven(mvn, url)
+
+
+if __name__ == '__main__':
+    url = sys.argv[1]
+    groupId = sys.argv[2]
+    artifactId = sys.argv[3]
+    applicationName = sys.argv[4]
+
+    mvn = "mvn archetype:generate -DgroupId=" + groupId \
+          + " -DartifactId=" + artifactId \
+          + " -DpackageName=com.spdb -Dversion=0.0.1-SNAPSHOT" \
+            " -DapplicationName=" + applicationName \
+          + " -DarchetypeArtifactId=maven-archetype-webapp " \
+            "-DinteractiveMode=false "
+    print(mvn)
+    if url is not None and groupId is not None and artifactId is not None and applicationName is not None:
+        result = getResult(mvn, url)
+        print(result)
