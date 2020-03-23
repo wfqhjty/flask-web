fec978d45e719384fb38f8978676b5fb24903d3f
wangfq <wfqtyhj@gmail.com>
2020-03-23 15:33:02 +0800

    format

--- spider/RequestsDemo.py
+++ spider/RequestsDemo.py
@@ -24,7 +24,8 @@ def download_allpicture(url, path):
     if not os.path.isdir(path):
         os.makedirs(path)
     headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'}
+        'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
+                      'Chrome/58.0.3029.110 Safari/537.36'}
     response = requests.get(url, headers=headers)
     # response = requests.get(url)
     print(response.text)
